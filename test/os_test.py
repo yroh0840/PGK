6f9c164b70b627e6971344c14e6a61c17fe0b358
yroh0840 <49275461+yroh0840@users.noreply.github.com>
2019-12-23 15:46:46 +0900

    Update os_test.py

--- test/os_test.py
+++ test/os_test.py
@@ -5,4 +5,4 @@ print('basename:', os.path.basename(__file__))
 print('dirname:', os.path.dirname(__file__))
 '''
 curdir = os.path.dirname(__file__)
-print('curdir: ', curdir)
+print('curdir: ', '../curdir')
