81721b9c9046a7825ed14c097effa5b024e2a2e7
yroh0840 <49275461+yroh0840@users.noreply.github.com>
2019-12-23 15:46:50 +0900

    Create dir_test.py

---
+++ test/dir_test.py
@@ -0,0 +1,5 @@
+import os
+
+curdir = os.path.dirname(__file__) # 現在のフォルダのパス取得
+print('curdir >>> ', curdir)
+image = tk.PhotoImage(file = ../curdir+'/material/backgrounds_02/background_girl02.png')
