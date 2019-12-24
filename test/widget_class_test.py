8c0be945184d6aa5c0bad236380a0fb0405778ca
yroh0840 <49275461+yroh0840@users.noreply.github.com>
2019-12-24 14:40:26 +0900

    Update widget_class_test.py

--- test/widget_class_test.py
+++ test/widget_class_test.py
@@ -12,7 +12,9 @@ class Application(tk.Frame):
         self.curdir = os.path.dirname(__file__) # 現在のフォルダのパス取得
         self.image = tk.PhotoImage(file = self.curdir+'/../material/backgrounds_02/background_girl02.png') # 画像のｲﾝｽﾀﾝｽ変数
         self.canvas = tk.Canvas(self, width=540, height=960, bg="white")
+        # self.canvas.place(x=0,y=0)
         self.canvas.pack()
+
         self.canvas.create_image(0,0,image=self.image, anchor=tk.NW)
 
         self.image_girl = tk.PhotoImage(file = self.curdir+'/../material/buttons&entrywindows/girl_01_invi_circle.png')
@@ -29,15 +31,13 @@ class Application(tk.Frame):
             self.button = tk.Button(self.frame, text=text, bg=color, width=4)
             self.button.grid(row=0, column=cn, sticky=self.sticky, padx='10', pady='10')
 
-
-        '''
         self.frame2 = tk.Frame(self, bg='lightgreen')
-        self.frame2.pack()
+        self.frame2.pack(side=tk.BOTTOM)
         for cn, (text, color) in enumerate([('A', 'magenta'), ('B', 'yellow'), ('C', 'SeaGreen'), ('D', 'LightSkyBlue')]):
-            self.button = tk.Button(self.frame2, text=text, bg=color, width=4)
-            self.button.grid(row=0, column=cn, sticky=self.sticky, padx='10', pady='10')
+            self.button2 = tk.Button(self.frame2, text=text, bg=color, width=4)
+            self.button2.grid(row=0, column=cn, sticky=self.sticky, padx='10', pady='10')
            
-        
+        '''
         self.frame_menu_bar = tk.Frame(self, bg='lightgreen')
         self.frame_menu_bar.pack()
         for cn, (text, color) in enumerate([('A', 'magenta'), ('B', 'yellow'), ('C', 'SeaGreen'), ('D', 'LightSkyBlue')]):
