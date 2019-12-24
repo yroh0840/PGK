54034ba8bbb9e101a09ebd088fe4b33f99ce1965
yroh0840 <49275461+yroh0840@users.noreply.github.com>
2019-12-24 11:37:28 +0900

    Update widget_class_test.py

--- test/widget_class_test.py
+++ test/widget_class_test.py
@@ -14,12 +14,18 @@ class Application(tk.Frame):
         self.canvas = tk.Canvas(self, width=540, height=960, bg="white")
         self.canvas.pack()
         self.canvas.create_image(0,0,image=self.image, anchor=tk.NW)
+        self.image = # 画像ファイル
+        self.img 
+        self.label_image = tk.Label(self, image=)
         self.label = tk.Label(self, text='飯いくぞ！')
-        self.label.place(x=20, y=20, width='100', height='50')     
+        self.label.place(x=100, y=20, width='100', height='50')     
         self.frame = tk.Frame(self, bg='lightgreen')
         self.frame.place(x=250, y=60, width='250', height='50')
-        #self.button = tk.Button(self, text='A', width=4, bg='gray')
-        #self.button.pack(side='left')
+        self.sticky = tk.W+tk.E+tk.N+tk.S
+        for cn, (text, color) in enumerate([('A', 'magenta'), ('B', 'yellow'), ('C', 'SeaGreen'), ('D', 'LightSkyBlue')]):
+            self.button = tk.Button(self.frame, text=text, bg=color, width=4)
+            self.button.grid(row=0, column=cn, sticky=self.sticky, padx='10', pady='10')
+        
 
 
 
