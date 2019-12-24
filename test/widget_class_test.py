dea096d5fb3bd4d156b8c43c02b83ec984d00cf8
yroh0840 <49275461+yroh0840@users.noreply.github.com>
2019-12-24 14:12:46 +0900

    Update widget_class_test.py

--- test/widget_class_test.py
+++ test/widget_class_test.py
@@ -14,24 +14,41 @@ class Application(tk.Frame):
         self.canvas = tk.Canvas(self, width=540, height=960, bg="white")
         self.canvas.pack()
         self.canvas.create_image(0,0,image=self.image, anchor=tk.NW)
-        self.image = # 画像ファイル
-        self.img 
-        self.label_image = tk.Label(self, image=)
+
+        self.image_girl = tk.PhotoImage(file = self.curdir+'/../material/buttons&entrywindows/girl_01_invi_circle.png')
+        self.label_image = tk.Label(self, width=50, height=50, image=self.image_girl)
+        self.label_image.place(x=20, y=20)
+
         self.label = tk.Label(self, text='飯いくぞ！')
         self.label.place(x=100, y=20, width='100', height='50')     
+
         self.frame = tk.Frame(self, bg='lightgreen')
         self.frame.place(x=250, y=60, width='250', height='50')
         self.sticky = tk.W+tk.E+tk.N+tk.S
         for cn, (text, color) in enumerate([('A', 'magenta'), ('B', 'yellow'), ('C', 'SeaGreen'), ('D', 'LightSkyBlue')]):
             self.button = tk.Button(self.frame, text=text, bg=color, width=4)
             self.button.grid(row=0, column=cn, sticky=self.sticky, padx='10', pady='10')
-        
 
+
+        '''
+        self.frame2 = tk.Frame(self, bg='lightgreen')
+        self.frame2.pack()
+        for cn, (text, color) in enumerate([('A', 'magenta'), ('B', 'yellow'), ('C', 'SeaGreen'), ('D', 'LightSkyBlue')]):
+            self.button = tk.Button(self.frame2, text=text, bg=color, width=4)
+            self.button.grid(row=0, column=cn, sticky=self.sticky, padx='10', pady='10')
+           
+        
+        self.frame_menu_bar = tk.Frame(self, bg='lightgreen')
+        self.frame_menu_bar.pack()
+        for cn, (text, color) in enumerate([('A', 'magenta'), ('B', 'yellow'), ('C', 'SeaGreen'), ('D', 'LightSkyBlue')]):
+            self.button_set = tk.Button(self.frame_menu_bar, text=text, bg=color, width=4)
+            self.button_set.grid(row=0, column=cn, sticky=self.sticky, padx='10', pady='10')
+        '''
 
 
 def run():
     root = tk.Tk()
-    root.geometry('540x960')
+    root.geometry('540x960+0+0')
     root.title('ゴハンゴ')
     app = Application(root)
     root.mainloop()
