5e40e3807de80cd8feb4ed740d2cd469f5bc920b
yroh0840 <49275461+yroh0840@users.noreply.github.com>
2020-01-06 15:59:09 +0900

    Update gurunaviMap.py

--- maps/gurunaviMap.py
+++ maps/gurunaviMap.py
@@ -1,25 +1,26 @@
 import requests
-import googlemaps
 import json
 
 #レストラン検索APIのURL
-Url = "https://api.gnavi.co.jp/RestSearchAPI/v3/"
- 
+Url = 'https://api.gnavi.co.jp/RestSearchAPI/v3/'
+keyid = 'f8538f6f177eaaa41226f9fc9a805897'
+
 #パラメータの設定
 params={}
-params["keyid"] = "f8538f6f177eaaa41226f9fc9a805897" #取得したアクセスキー
-params["latitude"] = "35.695861"
-params["longitude"] = "139.775018"
-params["range"] = "1"
+params['keyid'] = keyid
+params["latitude"] = "35.695861" # 緯度
+params["longitude"] = "139.775018" # 経度
+
+params["range"] = "1" # 範囲
+params["pref"] = "1" # 都道府県コード
+params["sort"] = "1" # 並び替え
+params["hit_per_page"] = "３" # response max value
+params["freeword"] =  # フリーワード検索
+praams["lunch"] = "1" # ランチ営業
+
 
 #リクエスト結果
-print(requests.get(Url, params).json())
+result_api = requests.get(url, params)
+result_api = result_api.json()
 
-
-'''
-class map():
-    def __init__(self, master=None):
-        super().__init__(master)
-
-    def 
-'''
+print(result_api)
--- test/widget_class_test.py
+++ test/widget_class_test.py
@@ -18,42 +18,43 @@ class Application(tk.Frame):
         # 一番下のボタンたち
         self.sticky = tk.W+tk.E+tk.N+tk.S
         self.bottom_frame = tk.Frame(self, bg='lightgreen')
-        self.bottom_frame.place(x=0, y=460, width='560', height='100')
+        self.bottom_frame.place(x=0, y=0, width='560', height='100')
         for column, (text, color) in enumerate([('A', 'magenta'), ('B', 'yellow'), ('C', 'SeaGreen')],):
             self.button = tk.Button(self.bottom_frame, text=text, bg=color, width='15', height='5',
                                     command=lambda:[self.parent_frame_1.place(x=0, y=0), self.bottom_frame.destroy()])
             self.button.pack(padx=20, side='left')
         # トークエリア
-        self.parent_frame_1 = tk.Frame(self, width='560', height='128')
+        #ttk.Style().configure('TP.TFrame', background='white')
+        self.parent_frame_1 = tk.Frame(self, bg='#fcf3e7', width='560', height='128')
         self.parent_frame_1.place(x=0,y=0)
-        self.parent_frame_1.config(bg="systemTransparent")#あとで機能を調べる
-        self.parent_frame_2 = tk.Frame(self, width='560', height='128')
+        #self.parent_frame_1.config(bg="systemTransparent")#あとで機能を調べる
+        self.parent_frame_2 = tk.Frame(self, bg='#fcf3e7', width='560', height='128')
         self.parent_frame_2.place(x=0,y=128)
-        self.parent_frame_2.config(bg="systemTransparent")#あとで機能を調べる
-        self.parent_frame_3 = tk.Frame(self, width='560', height='128')
+        #self.parent_frame_2.config(bg="systemTransparent")#あとで機能を調べる
+        self.parent_frame_3 = tk.Frame(self, bg='#fcf3e7', width='560', height='128')
         self.parent_frame_3.place(x=0,y=256)
-        self.parent_frame_3.config(bg="systemTransparent")#あとで機能を調べる
+        #self.parent_frame_3.config(bg="systemTransparent")#あとで機能を調べる
         # 一時的にparent_frameを消す
         self.parent_frame_1.place_forget()
         self.parent_frame_2.place_forget()
         self.parent_frame_3.place_forget()
 
         self.svar1 = tk.StringVar() 
-        self.svar1.set('成功だね！')
+        self.svar1.set('おい！')
         self.svar2 = tk.StringVar()
-        self.svar2.set('チョベリバってかーんじぃ！')
+        self.svar2.set('お前股間臭いで！')
         self.svar3 = tk.StringVar()
-        self.svar3.set('ゲッツ！')
+        self.svar3.set('そーだそーだ！')
 
         # トークエリア一段目
         self.image_girl1 = tk.PhotoImage(file = self.curdir+'/../material/buttons&entrywindows/girl_01_invi_circle.png')
-        self.label_image1 = tk.Label(self.parent_frame_1, image=self.image_girl1)
+        self.label_image1 = tk.Label(self.parent_frame_1, bg='#fcf3e7', image=self.image_girl1)
         self.label_image1.place(x=0, y=0) 
-        self.label_comment1 = tk.Label(self.parent_frame_1, textvariable=self.svar1, font='100')
+        self.label_comment1 = tk.Label(self.parent_frame_1, bg='#fcf3e7', textvariable=self.svar1, font='100')
         self.label_comment1.place(x=64, y=0, height=68, )
         self.svar1 = tk.StringVar()
         self.svar1.set('active')
-        self.frame_button1 = tk.Frame(self.parent_frame_1, width='280', height='64', bg='white')
+        self.frame_button1 = tk.Frame(self.parent_frame_1, width='280', height='64', bg='#fcf3e7')
         self.frame_button1.place(x=280, y=64)
         self.button_list1 = []
         for cn, (text, color) in enumerate([('A', 'magenta'), ('B', 'yellow'), ('C', 'SeaGreen'), ('D', 'LightSkyBlue')]):
@@ -72,11 +73,11 @@ class Application(tk.Frame):
             self.button_list1[cn].grid(row=0, column=cn, sticky=self.sticky, padx='10', pady='10')
         # トークエリア二段目
         self.image_girl2 = tk.PhotoImage(file = self.curdir+'/../material/buttons&entrywindows/girl_01_invi_circle.png')
-        self.label_image2 = tk.Label(self.parent_frame_2, image=self.image_girl2)
+        self.label_image2 = tk.Label(self.parent_frame_2, bg='#fcf3e7', image=self.image_girl2)
         self.label_image2.place(x=0, y=0)
-        self.label_comment2 = tk.Label(self.parent_frame_2, textvariable=self.svar2, font='100')
+        self.label_comment2 = tk.Label(self.parent_frame_2, bg='#fcf3e7', textvariable=self.svar2, font='100')
         self.label_comment2.place(x=64, y=0, height=68, )
-        self.frame_button2 = tk.Frame(self.parent_frame_2, width='280', height='64', bg='white')
+        self.frame_button2 = tk.Frame(self.parent_frame_2, width='280', height='64', bg='#fcf3e7')
         self.frame_button2.place(x=280, y=64)
         self.button_list2 = []
         for cn, (text, color) in enumerate([('A', 'magenta'), ('B', 'yellow'), ('C', 'SeaGreen'), ('D', 'LightSkyBlue')]):
@@ -97,11 +98,11 @@ class Application(tk.Frame):
         # トークエリア三段目
         self.image_girl3 = tk.PhotoImage(file = self.curdir+'/../material/buttons&entrywindows/girl_01_invi_circle.png')
         self.image_girl4 = tk.PhotoImage(file = self.curdir+'/../material/buttons&entrywindows/洗濯おばさん.png')
-        self.label_image3 = tk.Label(self.parent_frame_3, image=self.image_girl4)
+        self.label_image3 = tk.Label(self.parent_frame_3, bg='#fcf3e7', image=self.image_girl4)
         self.label_image3.place(x=0, y=0)
-        self.label_comment3 = tk.Label(self.parent_frame_3, textvariable=self.svar3, font='100')
+        self.label_comment3 = tk.Label(self.parent_frame_3, bg='#fcf3e7', textvariable=self.svar3, font='100')
         self.label_comment3.place(x=64, y=0, height=68, )  
-        self.frame_button3 = tk.Frame(self.parent_frame_3, width='280', height='64', bg='white')
+        self.frame_button3 = tk.Frame(self.parent_frame_3, width='280', height='64', bg='#fcf3e7')
         self.frame_button3.place(x=280, y=64)
         self.button_list3 = []
         for cn, (text, color) in enumerate([('A', 'magenta'), ('B', 'yellow'), ('C', 'SeaGreen'), ('D', 'LightSkyBlue')]):
@@ -149,9 +150,9 @@ class Application(tk.Frame):
 def run():
     root = tk.Tk()
     #root.wm_attributes("-topmost", True)
-    ##root.wm_attributes("-disabled", True)
-    ##root.wm_attributes("-transparentcolor", "white")
-    root.wm_attributes("-transparent", False) #あとでどんな機能か調べる
+    #root.wm_attributes("-disabled", True)
+    #root.wm_attributes("-transparent", True) #あとでどんな機能か調べる
+    #root.wm_attributes('-transparentcolor', 'white')
     root.geometry('540x960+0+0')
     root.title('ゴハンゴ')
     app = Application(root)
